1f4043d1469b4b8ceb1311c2097103bbf79dca91
Julka <j.a.czapiewska@gmail.com>
2022-12-03 22:10:37 +0100

    html - new task card

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
 import "./../styles/styles.css"
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCzo9nfQ6ITrSXSZ-2qrxRMQN59JDlwdiU",
+    authDomain: "noteapp-7ec7b.firebaseapp.com",
+    projectId: "noteapp-7ec7b",
+    storageBucket: "noteapp-7ec7b.appspot.com",
+    messagingSenderId: "889568994523",
+    appId: "1:889568994523:web:3dc8f8369405aff7de98fe"
+  };
+
+const app = initializeApp(firebaseConfig);
